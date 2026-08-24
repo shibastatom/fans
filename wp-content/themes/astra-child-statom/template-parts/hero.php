1cefc9965ea2c669a9c5823c66de2abea13b7d6d
shibastatom <shiba.deb@statom.co.uk>
2026-08-24 14:54:36 +0100

    button update

--- wp-content/themes/astra-child-statom/template-parts/hero.php
+++ wp-content/themes/astra-child-statom/template-parts/hero.php
@@ -15,10 +15,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 <section class="st-section-s1 bg-primary">
 	<div class="ast-container flex flex-col md:flex-row items-center gap-8">
 		<div class="page-hero__content md:w-1/2">
-			<p class="uppercase text-secondary">London Residential & Mixed-use development</p>
-			<h1 class="_heading !text-white uppercase">Building exceptional <span class="text-secondary">places</span></h1>
+			<p class="uppercase !text-secondary">London Residential & Mixed-use development</p>
+			<h1 class="_heading !text-white uppercase">Building exceptional <span class="text-secondary">places.</span></h1>
 			<p class="st-hero-text-lead color-text text-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			<button>Button</button>
+			<button type="button" class="st-btn-secondary">Secondary</button>
+			<button type="button" class="st-btn-primary">Primary</button>
 		</div>
 
 		<?php if ( has_post_thumbnail() ) : ?>
