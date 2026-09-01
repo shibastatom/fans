ab15ba81ba77a3d40cde76de642a51d67867b870
shibastatom <shiba.deb@statom.co.uk>
2026-09-01 11:05:56 +0100

    highlight span creation

--- wp-content/themes/astra-child-statom/template-parts/hero-designs/design-3.php
+++ wp-content/themes/astra-child-statom/template-parts/hero-designs/design-3.php
@@ -26,12 +26,12 @@ include get_stylesheet_directory() . '/template-parts/acf-blocks/partials/genera
 class="st-section-s2 bg-primary bg-cover bg-center bg-no-repeat h-[80vh] flex" 
 
 >
-	<div class="ast-container w-full relative flex flex-row md:flex-row items-stretch">
+	<div class="ast-container w-full relative flex flex-row md:flex-row items-stretch gap-8">
 		<!-- LEFT SIDE -->
 		<div class="page-hero__content flex-1 flex flex-col justify-center">
 			<div>
 				<p>Hero Design 3</p>
-				<h1 class="_heading !text-white uppercase"><?= $gpo_alt_title ?></h1>
+				<h1 class="_heading uppercase !text-secondary"><?= $gpo_alt_title ?></h1>
 				<p class="st-hero-text-lead"><?php echo esc_html( get_the_excerpt() ); ?></p>
 				<button type="button" class="st-btn-secondary">Learn more</button>
 			</div>
