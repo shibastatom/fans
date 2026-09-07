47143b1cb2ffbcd16fe4da15605f85afec35caa7
shibastatom <shiba.deb@statom.co.uk>
2026-09-07 11:51:59 +0100

    contact section css updates

--- wp-content/themes/astra-child-statom/template-parts/acf-blocks/partials/cta/design-1.php
+++ wp-content/themes/astra-child-statom/template-parts/acf-blocks/partials/cta/design-1.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <!-- <div class="<?= $gbo_container_width; ?> py-12"> -->
 	<div
-	class="<?= $gsc_cta_block_card['cta_card']['background_colours']; ?> st-bg-fade-primary! flex shadow-lg overflow-hidden"
+	class="<?= $gsc_cta_block_card['cta_card']['background_colours']; ?> st-bg-fade-primary! flex shadow-lg overflow-hidden st-parallax"
 	>
 		<div class="flex-3 py-16 px-8">
 			<!-- <h2 class="uppercase !text-white st-heading-reveal">Let's Discuss Your <span class="text-secondary">Next Project</span></h2> -->
