76c2366eb42245ddf21d396b42876bea10ab745a
shibastatom <shiba.deb@statom.co.uk>
2026-09-07 12:31:37 +0100

    esign 4 update

--- wp-content/themes/astra-child-statom/template-parts/hero-designs/design-4.php
+++ wp-content/themes/astra-child-statom/template-parts/hero-designs/design-4.php
@@ -19,18 +19,21 @@ global $hero_design_f_image;
 		class="absolute inset-0 bg-cover bg-center bg-no-repeat st-ken-burns"
 		style="background-image: url('https://chocolate-zebra-256559.hostingersite.com/wp-content/uploads/2026/09/74054.jpg');"
 	></div>
-	<div class="absolute inset-0 st-bg-vignette-primary opacity-70"></div>
+	<div class="absolute inset-0 st-bg-vignette-primary opacity-80"></div>
 
 	<div class="ast-container relative z-10 flex flex-col justify-between py-16 w-full">
 		<div class="">
-			<h1 class="_heading !text-white uppercase st-heading-reveal">Building <br>exceptional <span class="text-secondary"><br>places.</span></h1>
+			<h1 
+			class="_heading st-hero-heading !text-white uppercase st-heading-reveal"
+			style=""
+			>Building <br>exceptional <span class="text-secondary"><br>places.</span></h1>
 		</div>
 
-		<div class="max-w-md self-end">
-			<p class="st-hero-text-lead !text-white st-reveal">
+		<div class="max-w-lg self-end">
+			<p class="st-hero-text-lead-2 !text-white st-reveal">
 				<?php st_render_word_reveal( 'Fancourt Property Group develops outstanding residential and mixed-use communities across London; acquired, optimised and delivered without compromise.' ); ?>
 			</p>
-			<button type="button" class="st-btn-secondary mt-4">Learn more</button>
+			<a class="st-btn-secondary">Learn more</a>
 		</div>
 	</div>
 </section>
